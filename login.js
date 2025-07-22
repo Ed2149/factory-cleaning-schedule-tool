73952fae693a36a126a1d229f3015fbd3c55ad1c
Ed2149 <Edwindurk5@gmail.com>
2025-07-22 13:54:02 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,14 @@
+document.getElementById('login-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  if (email === 'manager@gearops.com' && password === 'admin123') {
+    window.location.href = 'manager-dashboard.html';
+  } else if (email === 'employee@gearops.com' && password === 'clean123') {
+    window.location.href = 'employee-dashboard.html';
+  } else {
+    document.getElementById('loginError').textContent = 'Invalid credentials.';
+  }
+});
 
