bdd904682577c7dd9c68ae96242c3254de0da639
Ed2149 <Edwindurk5@gmail.com>
2025-07-23 16:09:26 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -9,3 +9,12 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     role = Column(String, nullable=False)
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    assigned_to = Column(String, nullable=False)
+    status = Column(String, default="pending")
