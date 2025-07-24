cde9409d31288603e638ce214d4f15642017aa4b
Ed2149 <Edwindurk5@gmail.com>
2025-07-24 10:30:57 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -111,7 +111,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or your frontend URL
+    allow_origins=["https://deluxe-rabanadas-fa1ebc.netlify.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
